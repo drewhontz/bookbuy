fcf8b9706686d2a78745cced2ca625328a4a9798
Drew Hontz <athontz@gmail.com>
2016-09-20 17:43:54 -0700

    working version before adding auth

--- app.py
+++ app.py
@@ -1,64 +1,142 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, jsonify, redirect
+from flask import flash, session as login_session
+from crud import *
+import random, string
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def catalog():
-	pass
+    latest = show_latest()
+    genre = show_genres()
+    return render_template('landing.html', latest=latest, genre=genre)
 
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	pass
+    if request.method == 'POST':
+        return redirect('/')
+    else:
+        return render_template('login.html')
 
 
 @app.route('/JSON')
 def JSONcatalog():
-	pass
+    books = show_catalog()
+    return jsonify(BookList=[b.serialize for b in books])
 
 
-@app.route('/new', methods=['GET','POST'])
+@app.route('/new', methods=['GET', 'POST'])
 def newGenre():
-	pass
+    if request.method == 'POST':
+        name = request.form['name']
+        add_genre(name=name)
+        flash("New genre added!")
+        return redirect('/')
+    else:
+        g = show_genres()
+        return render_template('new_genre.html', genre=g)
 
 
-@app.route('/<string:genre_name>/edit', methods=['GET','POST'])
-def editGenre(genre_name):
-	pass
+@app.route('/<string:genre_id>/edit', methods=['GET', 'POST'])
+def editGenre(genre_id):
+    if request.method == 'POST':
+        edit = request.form['name']
+        edit_genre(gid=genre_id, name=edit)
+        flash("Genre editted!")
+        return redirect(url_for('showGenre', genre_id=genre_id))
+    else:
+        g = get_genre_by_id(genre_id)
+        genres = show_genres()
+        return render_template('edit_genre.html', ge=g, genres=genres)
 
 
-@app.route('/<string:genre_name>/delete', methods=['GET','POST'])
-def deleteGenre(genre_name):
-	pass
+@app.route('/<string:genre_id>/delete', methods=['GET', 'POST'])
+def deleteGenre(genre_id):
+    if request.method == 'POST':
+        delete_genre(genre_id)
+        flash("Genre removed")
+        return redirect('/')
+    else:
+        g = get_genre_by_id(genre_id)
+        return render_template('delete.html', name=g.name, genre=g)
 
 
-@app.route('/<string:genre_name>')
-def showGenre(genre_name):
-	pass
+@app.route('/<string:genre_id>')
+def showGenre(genre_id):
+    b = show_books(genre_id)
+    g = show_genres()
+    genre = get_genre_by_id(genre_id)
+    return render_template('genre_contents.html', books=b, genre=g,
+                           type=genre.name, id=genre.id)
 
 
-@app.route('/<string:genre_name>/new', methods=['GET','POST'])
-def addBook(genre_name):
-	pass
+@app.route('/<string:genre_id>/new', methods=['GET', 'POST'])
+def newBook(genre_id):
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        publisher = request.form['publisher']
+        price = request.form['price']
+        description = request.form['description']
+        picture = request.form['picture']
+        add_book(title=title, author=author, publisher=publisher, price=price,
+                 description=description, picture=picture, genre_id=genre_id)
+        flash("New book added")
+        return redirect(url_for('showGenre', genre_id=genre_id))
+    else:
+        g = get_genre_by_id(genre_id)
+        genre = show_genres()
+        return render_template('new_book.html', genre=g, genres=genre)
 
 
-@app.route('/<string:genre_name>/<string:book_title>')
-def showBook(genre_name, book_title):
-	pass
+@app.route('/<string:genre_id>/<string:book_id>')
+def showBook(genre_id, book_id):
+    b = get_book_by_id(book_id)
+    g = show_genres()
+    genre = get_genre_by_id(genre_id)
+    return render_template('item_contents.html', genre=g, book=b, type=genre)
 
 
-@app.route('/<string:genre_name>/<string:book_title>/edit', methods=['GET','POST'])
-def editBook(genre_name, book_title):
-	pass
+@app.route('/<string:genre_id>/<string:book_id>/edit',
+           methods=['GET', 'POST'])
+def editBook(genre_id, book_id):
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        publisher = request.form['publisher']
+        price = request.form['price']
+        description = request.form['description']
+        picture = request.form['picture']
+        edit_book(bid=book_id, title=title, author=author, publisher=publisher,
+                  price=price, description=description, picture=picture,
+                  genre_id=genre_id)
+        flash("Book edits made!")
+        return redirect(url_for('showBook', genre_id=genre_id,
+                                book_id=book_id))
+    else:
+        b = get_book_by_id(book_id)
+        genre = get_genre_by_id(b.genre)
+        genres = show_genres()
+        return render_template('edit_book.html', genre=genre.name, book=b,
+                               genres=genres)
 
 
-@app.route('/<string:genre_name>/<string:book_title>/delete', methods=['GET','POST'])
-def deleteBook(genre_name, book_title):
-	pass
+@app.route('/<string:genre_id>/<string:book_id>/delete',
+           methods=['GET', 'POST'])
+def deleteBook(genre_id, book_id):
+    if request.method == 'POST':
+        delete_book(book_id)
+        flash("Book deleted")
+        return redirect(url_for('showGenre', genre_id=genre_id))
+    else:
+        b = get_book_by_id(book_id)
+        g = get_genre_by_id(genre_id)
+        return render_template('delete.html', name=b.title, genre=g)
 
 
 if __name__ == '__main__':
-  app.secret_key = 'super_secret_key'
-  app.debug = True
-  app.run(host = '0.0.0.0', port = 5000)
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
---
+++ crud.py
@@ -0,0 +1,66 @@
+from db_config import Base, Genre, Book
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+def show_books(gid):
+	return session.query(Book).filter_by(genre=gid).all()
+
+def get_book_by_id(bid):
+	return session.query(Book).filter_by(id=bid).one()
+
+def show_genres():
+	return session.query(Genre).all()
+
+def get_genre_by_id(gid):
+	return session.query(Genre).filter_by(id=gid).one()
+
+def show_latest():
+	return session.query(Book).limit(5)
+
+def show_catalog():
+	return session.query(Book).all()
+
+def add_genre(name):
+	g = Genre(name=name)
+	session.add(g)
+	session.commit()
+
+def edit_genre(gid, name):
+	g = get_genre_by_id(gid)
+	g.name = name
+	session.add(g)
+	session.commit()
+
+def delete_genre(gid):
+	session.delete(get_genre_by_id(gid))
+	session.commit()
+
+def delete_book(bid):
+	session.delete(get_book_by_id(bid))
+	session.commit()
+
+def add_book(title, author, publisher, price, description, picture, genre_id):
+	b = Book(title=title, author=author, publisher=publisher, price=price,
+		description=description, picture=picture, genre=genre_id)
+	session.add(b)
+	session.commit()
+
+def edit_book(bid, title, author, publisher, price, description, picture, genre_id):
+	b = get_book_by_id(bid)
+	
+	b.title=title
+	b.author=author
+	b.publisher=publisher
+	b.price=price
+	b.description=description
+	b.picture=picture
+	b.genre=genre_id
+
+	session.add(b)
+	session.commit()
