8df696df091bf5ea2a1c6a4c0f1b45a16b4eca21
Drew Hontz <athontz@gmail.com>
2016-09-23 15:09:50 -0700

    PEP8'd all py code

--- crud.py
+++ crud.py
@@ -8,60 +8,73 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
 def show_books(gid):
-	return session.query(Book).filter_by(genre=gid).all()
+    return session.query(Book).filter_by(genre=gid).all()
+
 
 def get_book_by_id(bid):
-	return session.query(Book).filter_by(id=bid).one()
+    return session.query(Book).filter_by(id=bid).one()
+
 
 def show_genres():
-	return session.query(Genre).all()
+    return session.query(Genre).all()
+
 
 def get_genre_by_id(gid):
-	res = session.query(Genre).filter_by(id=gid).first()
-	return res
+    res = session.query(Genre).filter_by(id=gid).first()
+    return res
+
 
 def show_latest():
-	return session.query(Book).limit(5)
+    return session.query(Book).limit(5)
+
 
 def show_catalog():
-	return session.query(Book).all()
+    return session.query(Book).all()
+
 
 def add_genre(name):
-	g = Genre(name=name)
-	session.add(g)
-	session.commit()
+    g = Genre(name=name)
+    session.add(g)
+    session.commit()
+
 
 def edit_genre(gid, name):
-	g = get_genre_by_id(gid)
-	g.name = name
-	session.add(g)
-	session.commit()
+    g = get_genre_by_id(gid)
+    g.name = name
+    session.add(g)
+    session.commit()
+
 
 def delete_genre(gid):
-	session.delete(get_genre_by_id(gid))
-	session.commit()
+    session.delete(get_genre_by_id(gid))
+    session.commit()
+
 
 def delete_book(bid):
-	session.delete(get_book_by_id(bid))
-	session.commit()
+    session.delete(get_book_by_id(bid))
+    session.commit()
+
 
 def add_book(title, author, publisher, price, description, picture, genre_id):
-	b = Book(title=title, author=author, publisher=publisher, price=price,
-		description=description, picture=picture, genre=genre_id)
-	session.add(b)
-	session.commit()
+    b = Book(title=title, author=author, publisher=publisher, price=price,
+             description=description, picture=picture, genre=genre_id)
+    session.add(b)
+    session.commit()
 
-def edit_book(bid, title, author, publisher, price, description, picture, genre_id):
-	b = get_book_by_id(bid)
-	
-	b.title=title
-	b.author=author
-	b.publisher=publisher
-	b.price=price
-	b.description=description
-	b.picture=picture
-	b.genre=genre_id
 
-	session.add(b)
-	session.commit()
+def edit_book(bid, title, author, publisher, price, description, picture,
+              genre_id):
+    b = get_book_by_id(bid)
+
+    b.title = title
+    b.author = author
+    b.publisher = publisher
+    b.price = price
+    b.description = description
+    b.picture = picture
+    b.genre = genre_id
+
+    session.add(b)
+    session.commit()
--- db_config.py
+++ db_config.py
@@ -4,44 +4,46 @@ import datetime
 
 Base = declarative_base()
 
+
 class Genre(Base):
 
-	__tablename__ = 'genre'
-	id = Column(Integer, primary_key=True)
-	name = Column(String(25), nullable=False)
+    __tablename__ = 'genre'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(25), nullable=False)
 
-	@property
-	def serialize(self):
-		return {
-			'id' : self.id,
-			'name' : self.name
-		}
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
 
 class Book(Base):
 
-	__tablename__ = 'book'
-	id = Column(Integer, primary_key=True)
-	title = Column(String, nullable=False)
-	author = Column(String(30), nullable=False)
-	publisher = Column(String(25)) 
-	genre = Column(Integer, ForeignKey(Genre.id))
-	price = Column(Integer, nullable=False)
-	description = Column(Text, nullable=False)
-	picture = Column(String, nullable=False)
-	# created = Column(String, default=datetime.datetime.utcnow)
+    __tablename__ = 'book'
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    author = Column(String(30), nullable=False)
+    publisher = Column(String(25))
+    genre = Column(Integer, ForeignKey(Genre.id))
+    price = Column(Integer, nullable=False)
+    description = Column(Text, nullable=False)
+    picture = Column(String, nullable=False)
+    # created = Column(String, default=datetime.datetime.utcnow)
 
-	@property
-	def serialize(self):
-		return {
-			'id' : self.id,
-			'title' : self.title,
-			'author' : self.author,
-			'publisher' : self.publisher,
-			'genre' : self.genre,
-			'price' : self.price,
-			'description' : self.description,
-			'picture' : self.picture
-		}	
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'author': self.author,
+            'publisher': self.publisher,
+            'genre': self.genre,
+            'price': self.price,
+            'description': self.description,
+            'picture': self.picture
+        }
 
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.create_all(engine)
