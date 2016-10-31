9a689162e066320a61ed73db44d50922db8a1aea
Drew Hontz <athontz@gmail.com>
2016-10-31 15:56:34 -0700

    added full name to client_secret

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ import requests
 app = Flask(__name__)
 
 CLIENT_ID = json.loads(
-    open('./client_secret.json', 'r').read())['web']['client_id']
+    open('/var/www/BookBuy/BookBuy/client_secret.json', 'r').read())['web']['client_id']
 APPLICATION_NAME = "Book Buy Application"
 
 # Convenience function for checking if user logged in
